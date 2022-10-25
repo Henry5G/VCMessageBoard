176d734c304c13a4b623c2452d3334e8c2f7c43d
Henry <henrygrove2@Gmail.com>
2022-10-25 21:22:56 +0200

    New commit

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace VCMessageBoard
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            //New comment
         }
     }
 }

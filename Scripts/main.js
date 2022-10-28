fd4eacf797340054af4bbe752e661cfa94e7cddb
dannyp123 <dpienaar001@gmail.com>
2022-10-28 11:18:58 +0200

    Finished copying

---
+++ Scripts/main.js
@@ -0,0 +1,25 @@
+﻿function AlertName(name) {
+
+    alert('You clicked ' + name + "!");
+
+}
+
+function FacultyPage() {
+
+    document.location.href = "Home/Contact";
+    alert("Yes");
+}
+
+$(document).ready(function () {
+    $("#signin-button").click(function () {
+
+        $('#signin').toggleClass('centered');
+        $('#register').toggleClass('centered');
+    })
+
+    $("#register-button").click(function () {
+
+        $('#signin').toggleClass('centered');
+        $('#register').toggleClass('centered');
+    })
+});
